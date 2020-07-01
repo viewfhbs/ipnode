910c6d1615f866feb2ac868943e4f096793afab3
Forhad Chowdhury <findforhad.io@gmail.com>
2020-07-01 07:02:37 +0600

    Small Change

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-
+const interfaces = require("os").networkInterfaces();
 module.exports = () => {
+  let addresses = {};
 
-    const interfaces = require("os").networkInterfaces(); let addresses = {};
+  for (let interface in interfaces) {
+    for (let k2 in interfaces[interface]) {
+      const address = interfaces[interface][k2];
+      if (address.family === "IPv4" && !address.internal) {
+        addresses["MAC"] = address.mac;
+        addresses["IPv4"] = address.address;
+        addresses["NET_MASK"] = address.netmask;
+      }
+    }
+  }
 
-for (let interface in interfaces) { for (let k2 in interfaces[interface]) { const address = interfaces[interface][k2]; if (address.family === "IPv4" && !address.internal) { addresses["MAC"] = address.mac; addresses["IPv4"] = address.address; addresses["NET_MASK"] = address.netmask; } } }
-
-console.log(addresses);
-
-}
-
-
+  console.log(addresses);
+};
