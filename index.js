69c2c5a6d30217d4aefaf0cdf4f5656f93b988d0
Forhad Chowdhury <forhadhossenbabu@gmail.com>
2020-06-30 20:47:19 -0400

    Initial Released v1.1.9

---
+++ index.js
@@ -0,0 +1,12 @@
+
+module.exports = () => {
+
+    const interfaces = require("os").networkInterfaces(); let addresses = {};
+
+for (let interface in interfaces) { for (let k2 in interfaces[interface]) { const address = interfaces[interface][k2]; if (address.family === "IPv4" && !address.internal) { addresses["MAC"] = address.mac; addresses["IPv4"] = address.address; addresses["NET_MASK"] = address.netmask; } } }
+
+console.log(addresses);
+
+}
+
+
